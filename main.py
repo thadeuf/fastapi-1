7ca3d0118c30e0a96e3268d084a7fcef2a85cbb2
Thadeu <82273300+thadeuf@users.noreply.github.com>
2024-09-24 16:19:54 -0300

    download pdf

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from typing import Optional
-
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI
+import requests
 import pdfplumber
+from io import BytesIO
 
 app = FastAPI()
 
 @app.post("/extract-text")
-async def extract_text(file: UploadFile = File(...)):
-    # Abre o arquivo PDF
-    with pdfplumber.open(file.file) as pdf:
+async def extract_text(urlPdf: str):
+    # Faz o download do arquivo PDF a partir da URL
+    response = requests.get(urlPdf)
+    response.raise_for_status()  # Verifica se o download foi bem-sucedido
+    
+    # Abre o PDF usando o conteúdo baixado
+    with pdfplumber.open(BytesIO(response.content)) as pdf:
         text = ""
         # Extrai o texto de cada página
         for page in pdf.pages:
